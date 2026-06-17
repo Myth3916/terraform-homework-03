d24ccbee9d49e23ccbbb249b7bcb80fe9193c893
Sharov Oleg <os127@yandex.ru>
2026-06-18 01:36:21 +0300

    Добавлен README и каталог screenshots

---
+++ count-vm.tf
@@ -0,0 +1,35 @@
+resource "yandex_compute_instance" "web" {
+  count = 2
+
+  name               = "web-${count.index + 1}"
+  platform_id        = "standard-v3"
+  zone               = "ru-central1-a"
+  
+  scheduling_policy {
+    preemptible = true
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8kdq6d0p8sij7h5qe3"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  depends_on = [yandex_compute_instance.db]
+}
---
+++ for_each-vm.tf
@@ -0,0 +1,33 @@
+resource "yandex_compute_instance" "db" {
+  for_each = { for vm in var.each_vm : vm.vm_name => vm }
+
+  name               = each.value.vm_name
+  platform_id        = "standard-v3"
+  zone               = var.default_zone
+  
+  scheduling_policy {
+    preemptible = true
+  }
+
+  resources {
+    cores  = each.value.cpu
+    memory = each.value.ram
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8kdq6d0p8sij7h5qe3"
+      size     = each.value.disk_volume
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${local.ssh_public_key}"
+  }
+}
